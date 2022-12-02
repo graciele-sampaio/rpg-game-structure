c44bc9e978878856c6fe83e27985b5f53a5466bd
Graciele Sampaio <graciele-1996@hotmail.com>
2022-12-02 11:31:26 -0300

    requisito 6

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,16 @@
+import IEnergy from '../Energy';
+
+export default interface IFighter {
+  lifePoints: number;
+  strength: number;
+  defense: number;
+  energy?: IEnergy;
+  
+  attack(enemy: IFighter): void;
+
+  special?(enemy: IFighter): void;
+
+  levelUp(): void;
+
+  receiveDamage(attackPoints: number): number;
+}
